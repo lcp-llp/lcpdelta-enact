ace9349524ad2e3e37b860398b8295b31215baac
Jac Thomas (LCP) <Jac.Thomas@lcp.com>
2024-08-06 09:42:55 +0100

    Revert "Bearer token thread safety (#25)"
    This reverts commit 3d38f888fcc7b581e1ac8ac86a4e4088982bfda0.

--- src/lcp_delta/common/credentials_holder.py
+++ src/lcp_delta/common/credentials_holder.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import httpx
 import json
-import threading
 from ..enact.response_objects.usage_info import UsageInfo
 from . import constants
 
@@ -12,20 +11,9 @@ class CredentialsHolder:
     def __init__(self, username: str, public_api_key: str):
         self.username = username
         self.public_api_key = public_api_key
-        self._token_lock = threading.Lock()
-        self.get_bearer_token()
+        self.bearer_token = self.get_bearer_token()
 
-    @property
-    def bearer_token(self):
-        with self._token_lock:
-            return self._bearer_token
-
-    @bearer_token.setter
-    def bearer_token(self, value):
-        with self._token_lock:
-            self._bearer_token = value
-
-    def get_bearer_token(self):
+    def get_bearer_token(self) -> str:
         """Get the bearer token for authentication.
 
         This method sends a request to obtain a bearer token for authentication
@@ -35,6 +23,9 @@ class CredentialsHolder:
         Args:
             username `str`: The username for Enact authentication.
             public_api_key `str`: The public API key for Enact authentication.
+
+        Returns:
+            str: The bearer token obtained from the Enact API.
         """
 
         headers = {"Content-Type": "application/json", "cache-control": "no-cache"}
@@ -45,7 +36,7 @@ class CredentialsHolder:
         if response.status_code == 401 or (response.status_code >= 500 and response.status_code < 600):
             response = self.retry_request(headers, data)
 
-        self.bearer_token = response.text
+        return response.text
 
     def retry_request(self, headers, data):
         """Retry the request to obtain a valid bearer token.
