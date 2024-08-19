d2ab4c8b6c38de25854fbcdf0be28a90af876536
Joshua Alder <Joshua.Alder@lcp.com>
2024-08-19 11:54:44 +0100

    update plant tests

--- tests/integration/test_api_plants.py
+++ tests/integration/test_api_plants.py
@@ -1,6 +1,5 @@
 import pytest
 import time
-from datetime import date
 from tests.integration import enact_api_helper
 
 
@@ -10,21 +9,29 @@ def teardown_function():
 
 @pytest.mark.asyncio
 async def test_get_plants_by_fuel_and_country_async():
-    res = await enact_api_helper.get_plants_by_fuel_and_country_async("Biomass", "Gb")
-    assert res is not None
+    res = await enact_api_helper.get_plants_by_fuel_and_country_async("CCGT", "Gb")
+    assert res[0] == "T_BARK-1"
 
 
 def test_get_plants_by_fuel_and_country_sync():
-    res = enact_api_helper.get_plants_by_fuel_and_country("Biomass", "Gb")
-    assert res is not None
+    res = enact_api_helper.get_plants_by_fuel_and_country("CCGT", "Gb")
+    assert res[0] == "T_BARK-1"
 
 
 @pytest.mark.asyncio
 async def test_get_plant_details_by_id_async():
-    res = await enact_api_helper.get_plant_details_by_id_async("T_IRNPS-1")
-    assert res is not None
+    res = await enact_api_helper.get_plant_details_by_id_async("E_HAWKB-1")
+    assert res["data"]["plantName"] == "Hawkers Hill"
+    assert res["data"]["owner"] == "TagEnergy"
+    assert res["data"]["coordinates"] == [-2.20808700157723, 51.0137984995639]
+    assert res["data"]["fuel"] == "Battery"
+    assert res["data"]["zone"] == "Z17"
 
 
 def test_get_plant_details_by_id_sync():
-    res = enact_api_helper.get_plant_details_by_id("T_IRNPS-1")
-    assert res is not None
+    res = enact_api_helper.get_plant_details_by_id("E_HAWKB-1")
+    assert res["data"]["plantName"] == "Hawkers Hill"
+    assert res["data"]["owner"] == "TagEnergy"
+    assert res["data"]["coordinates"] == [-2.20808700157723, 51.0137984995639]
+    assert res["data"]["fuel"] == "Battery"
+    assert res["data"]["zone"] == "Z17"
