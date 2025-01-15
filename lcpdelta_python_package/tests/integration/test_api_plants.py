a6af425c72cccc55b3ebe453579c50af0902a8d8
Tom Debenham <tom.debenham@lcp.uk.com>
2025-01-15 21:16:32 +0000

    Add tests

--- lcpdelta_python_package/tests/integration/test_api_plants.py
+++ lcpdelta_python_package/tests/integration/test_api_plants.py
@@ -35,3 +35,23 @@ def test_get_plant_details_by_id_sync():
     assert res["data"]["coordinates"] == [-2.20808700157723, 51.0137984995639]
     assert res["data"]["fuel"] == "Battery"
     assert res["data"]["zone"] == "Z17"
+    
+@pytest.mark.asyncio
+async def test_get_plant_details_by_fuel_async():
+    batteries = await enact_api_helper.get_plant_details_by_fuel_async("Battery")
+    h_hill = next((x for x in batteries if x["plantName"] == "Hawkers Hill"), None)
+
+    assert h_hill["owner"] == "TagEnergy"
+    assert h_hill["coordinates"] == [-2.20808700157723, 51.0137984995639]
+    assert h_hill["fuel"] == "Battery"
+    assert h_hill["zone"] == "Z17"
+
+
+def test_get_plant_details_by_fuel_sync():
+    batteries = enact_api_helper.get_plant_details_by_fuel("Battery")
+    h_hill = next((x for x in batteries if x["plantName"] == "Hawkers Hill"), None)
+
+    assert h_hill["owner"] == "TagEnergy"
+    assert h_hill["coordinates"] == [-2.20808700157723, 51.0137984995639]
+    assert h_hill["fuel"] == "Battery"
+    assert h_hill["zone"] == "Z17"
