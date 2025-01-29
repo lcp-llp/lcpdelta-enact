bfd1f2fa491ffcc450339cd6e4b9a6e8dad90dc9
Tom Debenham <tom.debenham@lcp.uk.com>
2025-01-29 12:47:07 +0000

    remove print

--- lcpdelta_python_package/src/lcp_delta/enact/loader.py
+++ lcpdelta_python_package/src/lcp_delta/enact/loader.py
@@ -3,9 +3,7 @@ import importlib
 
 def load_endpoints():
     env = os.getenv("ENACT_ENV", "default")  # Read from an environment variable
-    print(env)
     module_name = f".configs.endpoints_{env}"
-    print(module_name)
     try:
         return importlib.import_module(module_name, package=__package__)
     except ModuleNotFoundError:
