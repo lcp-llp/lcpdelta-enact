957cbdb7b771fdf16f7d753a6cf384a32978af03
Billy Ogier <Billy.Ogier@lcp.com>
2025-06-16 15:16:43 +0100

    added method for api

--- lcpdelta_python_package/src/lcp_delta/enact/services/index_service.py
+++ lcpdelta_python_package/src/lcp_delta/enact/services/index_service.py
@@ -22,8 +22,50 @@ def generate_request(
         "SelectedGranularity": granularity,
     }
 
+def generate_gb_info_request(
+    index_id: str,
+) -> dict:
+    return {
+        "IndexId": index_id,
+    }
 
-def process_default_index_info_response(response: dict) -> pd.DataFrame:
+def generate_gb_request(
+    date_from: datetime,
+    date_to: datetime,
+    index_id: str,
+    normalisation="EuroPerKwPerYear",
+    granularity="Week",
+    show_profit = "false",
+    gas_price_assumption = "DayAheadForward",
+    market_price_assumption = "EpexDayAheadPrice",
+    account_for_availability_in_normalisation = "false",
+    include_wholesale_split = "false",
+    bm_split_out_option = None,
+    ancillary_revenue_type = "ByProduct",
+    group_dx = "false",
+    include_capacity_market = "true",
+    include_non_delivery_charges = "true",
+) -> dict:
+    date_from, date_to = convert_datetimes_to_iso(date_from, date_to)
+    return {
+        "From": date_from,
+        "To": date_to,
+        "IndexId": index_id,
+        "SelectedNormalisation": normalisation,
+        "SelectedGranularity": granularity,
+        "ShowProfit": show_profit,
+        "GasPriceAssumption": gas_price_assumption,
+        "MarketPriceAssumption": market_price_assumption,
+        "AccountForAvailabilityInNormalisation": account_for_availability_in_normalisation,
+        "IncludeWholesaleSplit": include_wholesale_split,
+        "BmSplitOutOption": bm_split_out_option,
+        "AncillaryProfitAggregation": ancillary_revenue_type,
+        "GroupDx": group_dx,
+        "IncludeCmRevenues": include_capacity_market,
+        "IncludeNonDeliveryCharges": include_non_delivery_charges,
+    }
+
+def process_index_info_response(response: dict) -> pd.DataFrame:
     return convert_embedded_list_to_df(response)
 
 def process_index_data_response(response: dict) -> pd.DataFrame:
