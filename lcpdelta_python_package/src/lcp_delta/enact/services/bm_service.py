5ced2279856982ba9823479e7125bd636f2808c9
Patrick Courts <Patrick.Courts@lcp.com>
2025-07-16 14:00:44 +0100

    sending date as isostring

--- lcpdelta_python_package/src/lcp_delta/enact/services/bm_service.py
+++ lcpdelta_python_package/src/lcp_delta/enact/services/bm_service.py
@@ -45,7 +45,7 @@ def process_by_search_response(response: dict):
     return pd.DataFrame(response["data"][1:], columns=response["data"][0])
 
 def generate_by_day_request(date: datetime, include_accepted_times: bool):
-
+    date = convert_datetime_to_iso(date)
     request_body = {"date": date}
     if include_accepted_times is not False:
         request_body["includeAcceptedTimes"] = "True"
