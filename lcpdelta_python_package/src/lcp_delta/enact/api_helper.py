222f4289596557f266751d7f778acc34b9aa0abf
Will Rowe (LCP Delta) <Will.Rowe@lcp.com>
2025-08-21 10:20:18 +0100

    JAO bids python package

--- lcpdelta_python_package/src/lcp_delta/enact/api_helper.py
+++ lcpdelta_python_package/src/lcp_delta/enact/api_helper.py
@@ -17,6 +17,7 @@ from lcp_delta.enact.services import news_table_service
 from lcp_delta.enact.services import nordpool_service
 from lcp_delta.enact.services import plant_service
 from lcp_delta.enact.services import series_service
+from lcp_delta.enact.services import jaobids_service
 
 class APIHelper(APIHelperBase):
     def _make_series_request(
@@ -1914,3 +1915,18 @@ class APIHelper(APIHelperBase):
         )
         response = await self._post_request_async(self.endpoints.DAY_AHEAD, request_body)
         return day_ahead_service.process_response(response)
+    
+    def get_jao_bids(
+            self, corridor: str, horizon: str, dayCET: str, bidPeriodStart: str = None
+    ) -> pd.DataFrame:
+        request_body = jaobids_service.generate_jao_bids_request(corridor, horizon, dayCET, bidPeriodStart)
+        response = self._post_request(self.endpoints.JAO_BIDS, request_body)
+        return jaobids_service.process_response(response)
+    
+    async def get_jao_bids_async(
+            self, corridor: str, horizon: str, dayCET: str, bidPeriodStart: str = None
+    ) -> pd.DataFrame:
+        """An asynchronous version of `get_jao_bids`."""
+        request_body = jaobids_service.generate_jao_bids_request(corridor, horizon, dayCET, bidPeriodStart)
+        response = await self._post_request(self.endpoints.JAO_BIDS, request_body)
+        return jaobids_service.process_response(response)
---
+++ lcpdelta_python_package/src/lcp_delta/enact/services/jaobids_service.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from datetime import datetime
+
+day_string = "%Y-%m-%d"
+date_time_string = "%Y-%m-%dT%H:%M:%SZ"
+
+def generate_jao_bids_request(corridor: str, horizon: str, dayCET: datetime, bidPeriodStart: datetime = None):
+
+    dayCET_str = datetime.strftime(dayCET, day_string)
+    
+    request_body = {"Corridor": corridor, "Horizon": horizon, "DayCET": dayCET_str}
+    if bidPeriodStart != None:
+        bidPeriodStart_str = datetime.strftime(bidPeriodStart, date_time_string)
+        request_body["BidPeriodStart"] = bidPeriodStart_str
+
+    return request_body
+
+def process_response(response: dict) -> pd.DataFrame:
+
+    df = pd.DataFrame(response.get("data", []))
+
+    if "dayCET" in df.columns:
+        df["dayCET"] = pd.to_datetime(df["dayCET"])
+    if "bidPeriodStart" in df.columns:
+        df["bidPeriodStart"] = pd.to_datetime(df["bidPeriodStart"])
+
+    return df
