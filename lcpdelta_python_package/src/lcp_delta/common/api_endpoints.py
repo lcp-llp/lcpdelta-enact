3b00d8bc94c04c900c46a5f90375f2ccde3f5272
Seb Stanbridge (LCP Delta) <Seb.Stanbridge@lcp.com>
2026-08-14 15:05:28 +0100

    adding endpoint to list

--- lcpdelta_python_package/src/lcp_delta/common/api_endpoints.py
+++ lcpdelta_python_package/src/lcp_delta/common/api_endpoints.py
@@ -87,6 +87,8 @@ class EnactApiEndpoints:
     def PLANT_INFO_BY_FUEL(self): return f"{self._base_endpoints.MAIN_BASE_URL}/EnactAPI/Plant/Data/PlantInfoByFuelType"
     @property
     def PLANT_IDS(self): return f"{self._base_endpoints.MAIN_BASE_URL}/EnactAPI/Plant/Data/PlantList"
+    @property
+    def PLANT_IDS(self): return f"{self._base_endpoints.MAIN_BASE_URL}/EnactAPI/Plant/Data/FuelList"
 
     @property
     def HOF(self): return f"{self._base_endpoints.MAIN_BASE_URL}/EnactAPI/HistoryOfForecast/Data_V2"
